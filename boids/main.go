af40c32d6c50238df83b7fdbd08030057ebd2931
petros <pit.trak@gmail.com>
2021-03-05 17:53:37 +0200

    Boid Synchronization with Readers-Writers Locks

--- boids/main.go
+++ boids/main.go
@@ -12,7 +12,7 @@ const (
 	screenWidth, screenHeight = 640, 360
 	boidCount                 = 500
 	viewRadius                = 13
-	adjRate                   = 0.015
+	adjRate                   = 0.7
 	windowTitle               = "Boids in a box"
 )
 
@@ -20,7 +20,7 @@ var (
 	green   = color.RGBA{10, 255, 50, 255}
 	boids   [boidCount]*Boid
 	boidMap [screenWidth + 1][screenHeight + 1]int
-	lock    = sync.Mutex{}
+	rWlock  = sync.RWMutex{}
 )
 
 type Game struct{}
