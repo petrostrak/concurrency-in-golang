c63099dc14566e5e389d1f5fbc43fb02deda83b3
petros <pit.trak@gmail.com>
2021-03-05 16:41:15 +0200

    fic calcAcceleration

--- boids/boid.go
+++ boids/boid.go
@@ -18,16 +18,16 @@ func (b *Boid) calcAcceleration() Vector2D {
 	avgVelocity := Vector2D{0, 0}
 	count := 0.0
 
-	for i := math.Max(lower.x, 0); i <= math.Min(upper.x, screenWidth); i ++ {
-		for j := math.Max(lower.y, 0); j <= math.Min(upper.y, screenHeight); j ++ {
+	for i := math.Max(lower.x, 0); i <= math.Min(upper.x, screenWidth); i++ {
+		for j := math.Max(lower.y, 0); j <= math.Min(upper.y, screenHeight); j++ {
 			if otherBoidId := boidMap[int(i)][int(j)]; otherBoidId != -1 && otherBoidId != b.id {
 				if dist := boids[otherBoidId].position.Distance(b.position); dist < viewRadius {
 					count++
 					avgVelocity = avgVelocity.Add(boids[otherBoidId].velocity)
 				}
 			}
+		}
 	}
-
 	accel := Vector2D{0, 0}
 	if count > 0 {
 		avgVelocity = avgVelocity.DivisionV(count)
