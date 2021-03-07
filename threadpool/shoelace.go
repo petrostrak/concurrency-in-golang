2622591ea788666bd2fe6bac9c17b83c93f8b061
petros <pit.trak@gmail.com>
2021-03-07 14:52:56 +0200

    threatpool shoelace example

--- threadpool/shoelace.go
+++ threadpool/shoelace.go
@@ -8,6 +8,7 @@ import (
 	"regexp"
 	"strconv"
 	"strings"
+	"sync"
 	"time"
 )
 
@@ -16,24 +17,32 @@ type Point2D struct {
 	y int
 }
 
-var (
-	r = regexp.MustCompile(`\((\d*),(\d*)\)`)
+const (
+	numberOfThreads int = 8
 )
 
-func findArea(pointsStr string) {
-	var points []Point2D
-	for _, p := range r.FindAllStringSubmatch(pointsStr, -1) {
-		x, _ := strconv.Atoi(p[1])
-		y, _ := strconv.Atoi(p[2])
-		points = append(points, Point2D{x, y})
-	}
+var (
+	r         = regexp.MustCompile(`\((\d*),(\d*)\)`)
+	waitGroup = sync.WaitGroup{}
+)
 
-	area := 0.0
-	for i := 0; i < len(points); i++ {
-		a, b := points[i], points[(i+1)%len(points)]
-		area += float64(a.x*b.y) - float64(a.y*b.x)
+func findArea(inputChannel chan string) {
+	for pointStr := range inputChannel {
+		var points []Point2D
+		for _, p := range r.FindAllStringSubmatch(pointStr, -1) {
+			x, _ := strconv.Atoi(p[1])
+			y, _ := strconv.Atoi(p[2])
+			points = append(points, Point2D{x, y})
+		}
+
+		area := 0.0
+		for i := 0; i < len(points); i++ {
+			a, b := points[i], points[(i+1)%len(points)]
+			area += float64(a.x*b.y) - float64(a.y*b.x)
+		}
+		fmt.Println(math.Abs(area) / 2.0)
 	}
-	fmt.Println(math.Abs(area) / 2.0)
+	waitGroup.Done()
 }
 
 func main() {
@@ -41,11 +50,18 @@ func main() {
 	dat, _ := ioutil.ReadFile(filepath.Join(absPath, "polygons.txt"))
 	text := string(dat)
 
+	inputChannel := make(chan string, 1000)
+	for i := 0; i < numberOfThreads; i++ {
+		go findArea(inputChannel)
+	}
+	waitGroup.Add(numberOfThreads)
 	start := time.Now()
 	for _, line := range strings.Split(text, "\n") {
 		// line := "(4,10),(12,8),(10,3),(2,2),(7,5)"
-		findArea(line)
+		inputChannel <- line
 	}
+	close(inputChannel)
+	waitGroup.Wait()
 	elapsed := time.Since(start)
 	fmt.Printf("Processing took %f \n", elapsed.Seconds())
 }
